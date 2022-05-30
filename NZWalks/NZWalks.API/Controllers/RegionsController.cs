95eddf33244e0d8a914fbd7637092bedc6758292
Ahmed0484 <a_elshazly_08@hotmail.com>
2022-05-30 18:47:50 +0200

    End of new Controller

---
+++ NZWalks/NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.DTO;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RegionsController : Controller
+    {
+        private readonly IRegionRepository repo;
+        private readonly IMapper mapper;
+
+        public RegionsController(IRegionRepository repo,IMapper mapper)
+        {
+            this.repo = repo;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]   
+        public async  Task<IActionResult> Index()
+        {
+            var regions = await this.repo.GetAllAsync();
+            /*var regionsDTO = new List<RegionDTO>();
+            regions.ToList().ForEach(region => regionsDTO.Add(
+                new RegionDTO
+                {
+                    Id=region.Id,
+                    Name=region.Name,
+                    Area=region.Area,
+                    Code=region.Code,
+                    Lat=region.Lat, 
+                    Long=region.Long,
+                    Population=region.Population
+                }
+                ));*/
+            var regionsDTO=mapper.Map<List<RegionDTO>>(regions);
+            return Ok(regionsDTO);
+        }
+    }
+}
---
+++ NZWalks/NZWalks.API/Repositories/RegionRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repositories
+{
+    public class RegionRepository : IRegionRepository
+    {
+        private readonly NZWalkDbContext ctx;
+
+        public RegionRepository(NZWalkDbContext ctx)
+        {
+            this.ctx = ctx;
+        }
+        public async Task<IEnumerable<Region>> GetAllAsync()
+        {
+            return await this.ctx.Regions.ToListAsync();
+        }
+    }
+}
